e7a4cdf9009e197f73f6dcaa760f654f45b27428
Jay <jhonglee@bu.edu>
2024-08-14 02:46:38 +0900

    implement tensornetwork grover algorithm

--- tensornetwork/tensornetwork_circuit.py
+++ tensornetwork/tensornetwork_circuit.py
@@ -12,6 +12,12 @@ H = np.array([[1, 1], [1, -1]], dtype=complex) / np.sqrt(2)
 S = np.array([[1, 0], [0, 1j]], dtype=complex)
 T = np.array([[1, 0], [0, np.exp(1j * np.pi / 4)]], dtype=complex)
 
+CNOT = np.zeros((2, 2, 2, 2), dtype=complex)
+CNOT[0][0][0][0] = 1
+CNOT[0][1][0][1] = 1
+CNOT[1][0][1][1] = 1
+CNOT[1][1][1][0] = 1
+
 class TensorNetworkCircuit(object):
     def __init__(self, num_qubits):
         self.num_qubits = num_qubits
@@ -30,6 +36,52 @@ class TensorNetworkCircuit(object):
             tn.connect(self.qubits[qubit], gate_node[index_of_gate])
             self.qubits[qubit] = gate_node[index_of_gate + len(apply_qubit)]
     
+    # TODO Implement this function
+    # Define the multi-controlled gate
+    def controls_target_gate_generator(self, gate, num_control_qubits):
+        # Define projectors |0><0| and |1><1|
+        P0 = np.array([[1, 0], [0, 0]])  # Projector for |0>
+        P1 = np.array([[0, 0], [0, 1]])  # Projector for |1>
+        
+        # define numer of cases
+        num_of_cases = 2 ** num_control_qubits
+        total_qubits = num_control_qubits + 1
+        
+        matrix = np.zeros((2 ** (num_control_qubits + 1), 2 ** (num_control_qubits + 1)), dtype=complex)
+ 
+        for index in range(num_of_cases):
+            # local variable for each cases matrix
+            term = np.eye(1)
+            order = 0
+            # cases box
+            control_state = [int(x) for x in format(index, f'0{num_control_qubits}b')]
+            for qubit in range(total_qubits):
+                if order < num_control_qubits :
+                    checker = control_state[order]
+                    if checker == 0:
+                        term = np.kron(term, P0)
+                    else:
+                        term = np.kron(term, P1)
+                    order += 1
+                else:
+                    if index == num_of_cases - 1:
+                        term = np.kron(term, gate)
+                    else:
+                        term = np.kron(term, I) 
+            
+            matrix += term
+        
+        matrix = matrix.flatten()
+        reshaped = []
+        for i in range(num_control_qubits + 1):
+            for _ in range(2):
+                reshaped.append(2)
+        matrix = matrix.reshape(reshaped)
+        return matrix
+    
+    # TODO 타켓 순서에 상관없이 적용이 가능한가?
+    # CX = 그냥 CX 게이트
+
     # Define the X gate
     def x(self, apply_qubit):
         with tn.NodeCollection(self.state_nodes):
@@ -61,10 +113,50 @@ class TensorNetworkCircuit(object):
         with tn.NodeCollection(self.state_nodes):
             self.apply_gate(T, apply_qubit)
     
+    ### 2-qubit Gates ###
+    # Define the CNOT gate
+    def cx(self, control_qubit, target_qubit):
+        with tn.NodeCollection(self.state_nodes):
+            CX = self.controls_target_gate_generator(X, 1)
+            control_qubit.append(target_qubit)
+            self.apply_gate(CX, control_qubit)
+    
+    # Define the CZ gate
+    def cz(self, control_qubit, target_qubit):
+        with tn.NodeCollection(self.state_nodes):
+            CZ = self.controls_target_gate_generator(Z, 1)
+            control_qubit.append(target_qubit)
+            self.apply_gate(CZ, control_qubit)
+    
+    def ccx(self, control_qubits, target_qubit):
+        with tn.NodeCollection(self.state_nodes):
+            CCX = self.controls_target_gate_generator(X, 2)
+            control_qubits.append(target_qubit)
+            self.apply_gate(CCX, control_qubits)
+    
+    def ccz(self, control_qubits, target_qubit):
+        with tn.NodeCollection(self.state_nodes):
+            CCZ = self.controls_target_gate_generator(Z, 2)
+            control_qubits.append(target_qubit)
+            self.apply_gate(CCZ, control_qubits)
+            
+    def mcx(self, control_qubits, target_qubit):
+        with tn.NodeCollection(self.state_nodes):
+            MCX = self.controls_target_gate_generator(X, len(control_qubits))
+            control_qubits.append(target_qubit)
+            self.apply_gate(MCX, control_qubits)
+    
+    def mcz(self, control_qubits, target_qubit):
+        with tn.NodeCollection(self.state_nodes):
+            MCZ = self.controls_target_gate_generator(Z, len(control_qubits))
+            control_qubits.append(target_qubit)
+            self.apply_gate(MCZ, control_qubits)
+    
     # Define simulation run
     def run(self):
         self.result = tn.contractors.optimal(self.state_nodes, output_edge_order=self.qubits)
     
+    # Define the convert function of matrix state to qubits
     def state_to_qubits(self):
         result_statevector = self.result.tensor.flatten()
         result = ""
@@ -73,8 +165,24 @@ class TensorNetworkCircuit(object):
                 result += f'{result_statevector[i]:.2f} * |{i:0{self.num_qubits}b}> + '
         return result[:-2]
     
-    def state_representation(self):
-        result_tensor = self.result.tensor
+    # Define the top possible qubit states
+    def top_possible_qubit_states(self):
+        num_qubits = self.num_qubits
+        max_prob = 0
+        result = []
+        result_statevector = self.result.tensor.flatten()
+        for i in range(len(result_statevector)):
+            if np.round(np.abs(result_statevector[i]), decimals=5) > max_prob:
+                max_prob = np.round(np.abs(result_statevector[i]), decimals=5)
+                result = []
+                result.append(f'|{i:0{num_qubits}b}>')
+            elif np.round(np.abs(result_statevector[i]), decimals=5) == max_prob:
+                result.append(f'|{i:0{num_qubits}b}>')
+        return result   
+    
+    # TODO
+    # Create target and control gate apply function
+    
         
 
 
--- tensornetwork/tensornetwork_grover.py
+++ tensornetwork/tensornetwork_grover.py
@@ -2,21 +2,75 @@ import tensornetwork as tn
 from tensornetwork_circuit import *
 import numpy as np
 
+# INITIALIZATION OF STATEVECTOR QUBITS
+# Define the statevector qubits
+def create_statevector_qubits(TensorNetworkCircuit):
+    statevector = np.zeros(2**n, dtype=complex)
+    statevector[0] = 1
+    return statevector
+
+def grover_cirquit(num_qubits, marked_states):
+    # Initialize the statevector qubits
+    tn_circuit = TensorNetworkCircuit(num_qubits)
+    
+    # Apply H-gate to each qubit
+    for qubit in range(num_qubits):
+        tn_circuit.h([qubit])
+    
+    # Apply the Oracle
+    # TODO Create the oracle circuit
+    for mid, target in enumerate(marked_states):
+        # Apply the X gate to non zero marked states qubits
+        zero_inds = [ind for ind in range(number_of_qubits) if target[ind] == '0']
+        if zero_inds != []:
+            for i in range(len(zero_inds)):
+                tn_circuit.x([zero_inds[i]])
+        # mcz
+        tn_circuit.mcz(list(range(num_qubits - 1)), num_qubits - 1) # multi-controlled-x gate
+        # Apply the X gate to non zero marked states qubits
+        if zero_inds != []:
+            for i in range(len(zero_inds)):
+                tn_circuit.x([zero_inds[i]])
+
+    # Apply the diffuser
+    for qubit in range(num_qubits):
+        tn_circuit.h([qubit])
+    for qubit in range(num_qubits):   
+        tn_circuit.x([qubit])
+    tn_circuit.h([num_qubits-1])
+    # Apply the multi-controlled-x gate to the qubits
+    tn_circuit.mcx(list(range(num_qubits-1)), num_qubits-1)
+    tn_circuit.h([num_qubits-1])
+    for qubit in range(num_qubits):
+        tn_circuit.x([qubit])
+    for qubit in range(num_qubits):  
+        tn_circuit.h([qubit])
+    
+    return tn_circuit
 
 # Define the number of qubits
-number_of_qubits = 3
+number_of_qubits = 4
 
 # Define the marked state
-marked_states = ['101', '110']
-
-tn_cirucit = TensorNetworkCircuit(number_of_qubits)
+marked_states = ['1010', '1110']
+    
+tn_cirucit = grover_cirquit(number_of_qubits, marked_states)
 # print("number of qubits : ", tn_cirucit.num_qubits, "\n")
 # print("`state_nodes : ", tn_cirucit.state_nodes, "\n")
 # print("circuit qubits : ", tn_cirucit.qubits, "\n")
 
-tn_cirucit.h([1])
 tn_cirucit.run()
-result = tn_cirucit.result
 
+print("Marked_states : ", marked_states, "\n")
 print("state : ")
 print(tn_cirucit.state_to_qubits(), "\n")
+print("Top possible qubit states : ")
+print(tn_cirucit.top_possible_qubit_states(), "\n")
+
+# TODO
+# Create the initalization superposition circuit
+# Create the oracle circuit
+# Create the diffuser circuit
+# Create the grover cirquit
+# Run the grover cirquit
+
